5f6c8749eb5df303a30b6533832c4af22889a5e2
XAuBAH <arahmaev@mail.ru>
2022-01-25 04:58:44 +0300

    Add files via upload

--- scripts/functions.js
+++ scripts/functions.js
@@ -1217,7 +1217,7 @@ curcontent["serv"] = {
 curcontent["Oplata"] = {
 	xhead: 'Пополнение баланса',
 	xcon: `<div class="xbox oplata_info">
-<form onsubmit="return qiwiHandler(event)" class="qiwi-inp-form" id="qiwi-inp-form" method="get" target="_blank" action="https://oplata.qiwi.com/create">
+<!-- <form onsubmit="return qiwiHandler(event)" class="qiwi-inp-form" id="qiwi-inp-form" method="get" target="_blank" action="https://oplata.qiwi.com/create">
 <label for="qiwi-donation-amount" class="qiwi-inp-main">
 	<div class="qiwi-widget-title qiwi-title-1">QIWI или Карты
 		<p>с самой минимальной комиссией</p>
@@ -1240,7 +1240,7 @@ curcontent["Oplata"] = {
             <div class="payment-type payment-block-visa"></div><div class="payment-type payment-block-mastercard"></div><div class="payment-type payment-block-mir"></div><div class="payment-type payment-block-qiwi"></div>
 	</div>
 	</label>
-</form>
+</form> -->
 <!--<form onsubmit="return unitpayHandler(event)" class="qiwi-inp-form" id="unitpay-inp-form" method="get" target="_blank" action="https://unitpay.money/pay/407453-95885/card">
 <div class="qiwi-inp-main" id="qiwi-inp-unitpay">
 	<div class="qiwi-widget-title qiwi-title-2">Моб. платежи или Иностранные карты</div>
@@ -1259,7 +1259,7 @@ curcontent["Oplata"] = {
 </form>-->
 <form onsubmit="return centHandler(event)" class="qiwi-inp-form" id="cent-inp-form" method="get" target="_blank" action="/api/cent.php">
 <label for="cent-donation-amount" class="qiwi-inp-main" id="qiwi-inp-cent">
-	<div class="qiwi-widget-title qiwi-title-2">Иностранные карты<br>или Apple Pay</div>
+	<div class="qiwi-widget-title qiwi-title-2">QIWI или карты</div>
 	<div class="qiwi-inp-box">
 			<label for="cent-donation-amount" class="qiwi-label">Cумма</label>
 			<div class="qiwi-rub">₽</div>
@@ -1272,7 +1272,7 @@ curcontent["Oplata"] = {
 		<button class="qiwi-submit-main" id="cent-submit-main" width="159px" type="submit">Оплатить</button>
 	</div>
 	<div class="payment-block">
-        <div class="payment-type payment-block-cent"></div><div class="payment-type payment-block-applepay"></div>
+        <div class="payment-type payment-block-qiwi"></div><div class="payment-type payment-block-cent"></div><div class="payment-type payment-block-applepay"></div>
 	</div>
 	</label>
 </form>
