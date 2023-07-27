8bb56b7237eb7fbc11e98ebf6d83867b14c8795a
XAuBAH <arahmaev@mail.ru>
2023-07-27 23:50:55 +0300

    Add files via upload

--- scripts/ruststore/vendor.js
+++ scripts/ruststore/vendor.js
@@ -59,7 +59,7 @@
         s.async = !0,
         s.timeout = 12e4,
         t.nc && s.setAttribute("nonce", t.nc),
-        s.src = t.p + "" + e + ".min.js?8";
+        s.src = t.p + "" + e + ".js?9";
         var u = setTimeout(n, 12e4);
         return s.onerror = s.onload = n,
         a.appendChild(s),
